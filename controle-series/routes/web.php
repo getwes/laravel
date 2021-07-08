6c6f8772de52bb87c8806a88e056c903c04bd4f1
getwes <wesleyfreire1707@gmail.com>
2021-07-07 22:04:48 -0300

    atualização

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+Route::get('/ola', function() {
+    echo "ola mundo !!!";
 });
+
+//route é a rota get é o localhost "http"
