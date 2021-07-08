1acf5b8c447ed371dd951d2c22673afc9433e7be
getwes <wesleyfreire1707@gmail.com>
2021-07-07 22:20:40 -0300

    atualização

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -12,13 +12,31 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//route é a rota get é o localhost "http"
 //Route::get('/', function () {
     //return view('welcome');
 //});
 
-Route::get('/ola', function() {
-    echo "ola mundo !!!";
+Route::get('/series', function() {
+    $series = [
+        'the 100',
+        'orange is the new black',
+        'grey\'s anatomy' ,
+        'lucifer',
+        'elite',
+        'la casa de papel',
+        'vis a vis',
+        'teen wolf',
+        'shadowhunters'
+
+    ];
+
+    $html = "<ul>";
+foreach ($series as $serie){
+    $html .="<li>$serie</li>";
+}
+$html .= "</ul>";
+
+echo $html;
 });
 
-//route é a rota get é o localhost "http"
