1100efc342f7a4eab5f5f2774fccf03122776d67
getwes <wesleyfreire1707@gmail.com>
2021-07-10 01:01:07 -0300

    atualização

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -13,30 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //route é a rota get é o localhost "http"
-//Route::get('/', function () {
-    //return view('welcome');
-//});
-
-Route::get('/series', function() {
-    $series = [
-        'the 100',
-        'orange is the new black',
-        'grey\'s anatomy' ,
-        'lucifer',
-        'elite',
-        'la casa de papel',
-        'vis a vis',
-        'teen wolf',
-        'shadowhunters'
-
-    ];
-
-    $html = "<ul>";
-foreach ($series as $serie){
-    $html .="<li>$serie</li>";
-}
-$html .= "</ul>";
-
-echo $html;
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/series', 'seriescontroller@listarseries' ) ;
+ 
