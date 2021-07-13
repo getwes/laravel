2bb303252c21cbecdbaf93aa48a6128b86d088c6
getwes <wesleyfreire1707@gmail.com>
2021-07-13 16:32:07 -0300

    atualização

--- controle-series/resources/views/series/index.php
+++ controle-series/resources/views/series/index.php
@@ -1,21 +1,24 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<!doctype html>
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>controle de series</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Controle de Séries</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 <body>
-<body>
     <div class="container">
-         <ul class="list-group">
-             <?php foreach ($series as $serie): ?>
-             <li class="list-group-intem"><?= $serie; ?></li>
-             <?php endforeach; ?>
-         </ul>
+       <div class="jumbotron">
+          <h1>Séries</h1>
     </div>
-</body>
+             <ul class="list-group">
+                <?php foreach ($series as $serie): ?>
+                <li class="list-group-item"><?= $serie; ?></li>
+                <?php endforeach; ?>
+             </ul>
+    </div>
 </body>
 </html>
