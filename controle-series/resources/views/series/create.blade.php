5fdbff669a1ee5b1026acb87f6904694cd7facf9
getwes <wesleyfreire1707@gmail.com>
2021-08-18 21:49:53 -0300

    atualização

--- controle-series/resources/views/series/create.blade.php
+++ controle-series/resources/views/series/create.blade.php
@@ -17,10 +17,18 @@
  
     <form method="post">
         @csrf
-        <div class="form-group">
-            <label for="nome">name</label>
-            <input type="text" class="form-control" name="nome" id="nome">
+    <div class="form-group">
+         <div class="col col-8">
+             <label for="nome">name</label>
+             <input type="text" class="form-control" name="nome" id="nome">
+         </div>
+
+         <div class="col col-8">
+            <label for="nome">numero de temporadas</label>
+            <input type="text" class="form-control" name="qtd_temporadas" id="nome">
         </div>
+
+    </div>
 
         <button class="btn btn-primary ">adicionar</button>
 
