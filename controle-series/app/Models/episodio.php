9bb77a56cbbfee6200f931d8518ec61004ef5cb6
getwes <wesleyfreire1707@gmail.com>
2021-08-31 20:42:35 -0300

    atualização

--- controle-series/app/Models/episodio.php
+++ controle-series/app/Models/episodio.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class episodio extends Model
 {
+    protected $fillable = ['numero'];
+    public$timestamps = false;
    public function temporada ()
    {
        return $this->belongto(temporada::class);
