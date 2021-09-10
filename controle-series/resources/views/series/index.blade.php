eb9cabc9df7ff85a820d139be536aff1fd0f3b89
getwes <wesleyfreire1707@gmail.com>
2021-09-09 21:55:42 -0300

    add botão de link no index

--- controle-series/resources/views/series/index.blade.php
+++ controle-series/resources/views/series/index.blade.php
@@ -12,24 +12,23 @@ series
 @endif
     <a href="{{route('form_criar_serie')}}" class="btn btn-dark mb-2">adicionar</a>
 
-    <ul class="list-group">
-        @foreach($series as $serie)
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            {{ $serie->nome }}
-    
-            <span class="d-flex">
-                <a href="/series/{{ $serie->id }}/temporadas" class="btn btn-info btn-sm mr-1">
-                    <i class="fas fa-external-link-alt"></i>
-                </a>
-                <form method="post" action="/series/{{ $serie->id }}"
-                      onsubmit="return confirm('Tem certeza que deseja remover {{ addslashes($serie->nome) }}?')">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger btn-sm">
-                        <i class="far fa-trash-alt"></i>
-                    </button>
-                </form>
-            </span>
-        </li>
-        @endforeach
-    </ul>
+             <ul class="list-group">
+                 @foreach($series as $serie)
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    {{$serie->nome }}
+                    <span class="d-flex">
+                        <a href="/series/{{ $serie->id }}/temporadas" class="btn btn-info btn-sm mr-1">
+                            <i class="fas fa-external-link-alt"></i>
+                        </a>
+                    <form method="post" action="/series/{{ $serie->id}}"
+                         onsubmit="return confirm('Tem certeza que deseja remover {{ addslashes( $serie->nome )}}?')">
+                        @csrf
+                        @method('delete')
+                        <button class="btn btn-danger btn-sm">
+                            <i class="fas fa-trash-alt"></i></button>
+                    </form>
+                </li>
+                 @endforeach
+             </ul>
+@endsection             
+ 
