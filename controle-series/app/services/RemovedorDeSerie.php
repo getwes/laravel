bac0758145632bdbcf9bd2baa60a8c8faa3401d5
getwes <wesleyfreire1707@gmail.com>
2021-10-05 17:00:01 -0300

    erro resolvido na questão da  transaction

--- controle-series/app/services/RemovedorDeSerie.php
+++ controle-series/app/services/RemovedorDeSerie.php
@@ -8,9 +8,10 @@ use Illuminate\Support\Facades\DB;
 
 class RemovedorDeSerie
 {
-    public function removerSerie(int $serieId): string
+    public function removerSerie(int $serieId)//: string Public function removerserie(): string <——- aqui depois dos : pontos vc declara o tipo de retorno do método/E no seu caso tá especificado que retorna string/Se não retornar nada ele vai reclamar .. que deve ter sido o caso //
     {
         $nomeSerie = '';
+        //recebe uma função e tudo que tiver nessa função vai estar dentro de uma transação só//
         DB::transaction( function () use ($serieId, &$nomeSerie){
 
             $serie = Serie::find($serieId);
@@ -29,3 +30,4 @@ class RemovedorDeSerie
 
     }
 }
+?>
