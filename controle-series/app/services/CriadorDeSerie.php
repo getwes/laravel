d80489dc07ae7a8ed3087fb04f1eaa55cffdcf6d
getwes <wesleyfreire1707@gmail.com>
2021-10-05 18:44:35 -0300

    refatorando criador de serie

--- controle-series/app/services/CriadorDeSerie.php
+++ controle-series/app/services/CriadorDeSerie.php
@@ -4,26 +4,49 @@ namespace App\Services;
 
 use App\Models\Serie;
 
+use Illuminate\Support\Facades\DB;
+
 class CriadorDeSerie
 {
 
-    public function criarSerie(string $nomeSerie, int $qtdTemporada, int $epPorTemporada) : Serie
+    public function criarSerie(
+        string $nomeSerie,
+        int $qtdTemporadas,
+        int $epPorTemporada
+    ) : Serie
     {
-        $serie = Serie::create(['nome' => $nomeSerie]);
-        $qtdTemporadas = $qtdTemporada;
-        for ($i = 0; $i <= $qtdTemporadas; $i++) {
-            $temporada = $serie->temporadas()->create(['numero' => $i]);
+        $serie = null;
+        DB::transaction(function () use ($nomeSerie, $qtdTemporadas, $epPorTemporada, &$serie)
+        {
+            $serie = Serie::create(['nome' => $nomeSerie]);
+            $this->criaTemporadas($qtdTemporadas, $epPorTemporada, $serie);
 
-            for ($j = 1; $j <= $epPorTemporada; $j++) {
-                $temporada->episodios()->create(['numero' => $j]);
-            }
-        }
+        });
 
         return $serie;
 
     }
-}
 
+    private function criaTemporadas(int $qtdTemporadas, int $epPorTemporada, Serie $serie)
+    {
+        for ($i = 0; $i <= $qtdTemporadas; $i++) {
+            $temporada = $serie->temporadas()->create(['numero' => $i]);
+
+            $this->criaEpisodios($epPorTemporada, $temporada);
+        }
+    }
+
+
+
+    private function criaEpisodios(int $epPorTemporada, \Illuminate\Database\Eloquent\Model $temporada): void
+
+    {
+
+        for ($j = 1; $j <= $epPorTemporada; $j++) {
+            $temporada->episodios()->create(['numero' => $j]);
+        }
+    }
+}
 
 
 
