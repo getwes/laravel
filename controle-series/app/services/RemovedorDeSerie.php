b6f15de479bd6d0e9e9ccbd65979aa841dee19a4
getwes <wesleyfreire1707@gmail.com>
2021-10-05 17:17:48 -0300

    refatorando comandos do removedor de series, episodios,e temporadas

--- controle-series/app/services/RemovedorDeSerie.php
+++ controle-series/app/services/RemovedorDeSerie.php
@@ -8,26 +8,44 @@ use Illuminate\Support\Facades\DB;
 
 class RemovedorDeSerie
 {
-    public function removerSerie(int $serieId)//: string Public function removerserie(): string <——- aqui depois dos : pontos vc declara o tipo de retorno do método/E no seu caso tá especificado que retorna string/Se não retornar nada ele vai reclamar .. que deve ter sido o caso //
+    public function removerSerie(int $serieId): string
     {
         $nomeSerie = '';
-        //recebe uma função e tudo que tiver nessa função vai estar dentro de uma transação só//
         DB::transaction( function () use ($serieId, &$nomeSerie){
 
             $serie = Serie::find($serieId);
             $nomeSerie = $serie->nome;
-            $serie->temporadas->each(function (Temporada $temporada) {
-                $temporada->episodios()->each(function(Episodio $episodio) {
-                    $episodio->delete();
-                });
-                $temporada->delete();
 
-            });
+
+
+
+            $this->removerTemporadas($serie);
             $serie->delete();
 
-            return $nomeSerie;
+        });
+
+        return $nomeSerie;
+
+    }
+
+    private  function removerTemporadas(Serie $serie): void
+    {
+        $serie->temporadas->each(function (Temporada $temporada) {
+
+
+           $this->removerEpisodios($temporada);
+           $temporada->delete();
+
+        });
+
+
+    }
+
+    private function removerEpisodios(Temporada $temporada): void
+    {
+        $temporada->episodios()->each(function (Episodio $episodio) {
+            $episodio->delete();
         });
 
     }
 }
-?>
