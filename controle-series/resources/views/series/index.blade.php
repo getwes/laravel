ef08ba9b2279673c85230abbcb385d11269f76e3
getwes <wesleyfreire1707@gmail.com>
2021-10-05 19:11:55 -0300

    add um botão para frazer edição dos nome das series

--- controle-series/resources/views/series/index.blade.php
+++ controle-series/resources/views/series/index.blade.php
@@ -15,8 +15,21 @@ series
              <ul class="list-group">
                  @foreach($series as $serie)
                 <li class="list-group-item d-flex justify-content-between align-items-center">
-                    {{$serie->nome }}
+                    <span id="nome-serie-{{ $serie->id }}">{{ $serie->nome }}</span>
+
+                    <div class="input-group w-50" hidden id="input-nome-serie-{{ $serie->id }}">
+                        <input type="text" class="form-control" value="{{ $serie->nome }}">
+                        <div class="input-group-append">
+                            <button class="btn btn-primary" onclick="editarSerie({{ $serie->id }})">
+                                <i class="fas fa-check"></i>
+                            </button>
+                            @csrf
+                        </div>
+                    </div>
                     <span class="d-flex">
+                        <button class="btn btn-info btn-sm mr-1" onclick="toggleInput({{ $serie->id }})">
+                            <i class="fas fa-edit"></i>
+                        </button>
                         <a href="/series/{{ $serie->id }}/temporadas" class="btn btn-info btn-sm mr-1">
                             <i class="fas fa-external-link-alt"></i>
                         </a>
